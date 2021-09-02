4ae82c0529ad9d2f9d75ca3f73cde629d6a9297a
Tanvir Tanjum Shourav <39719570+tanvirtanjum@users.noreply.github.com>
2021-09-02 17:09:41 +0600

    Home

--- controllers/Public/HomeController.js
+++ controllers/Public/HomeController.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     var base_URL = "http://127.0.0.1:5500";
 
     $('#topLayout').load("../Layouts/_SharedLayout.html");
-    $("#schoolName").typer(['KFT Collegiate School', 'কেএফটি কলেজিয়েট স্কুল']);
-        
+
+    $("#tittleName").typer(['KFT-Hazi Abdul Kader Molla-Fatema Begum Trust', 'KFT Collegiate School', 'কেএফটি কলেজিয়েট স্কুল']);
+  
+    $("#mottoText").typer(['"Dream for a better society"', '"Faith | Confidence | Integrity | Creativity"', '"বিশ্বাস | আত্মবিশ্বাস | অখণ্ডতা | সৃজনশীলতা"']);
 });
