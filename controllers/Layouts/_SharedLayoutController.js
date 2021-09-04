ee42fb197b9cc35104e89590bf22ed1076697bf1
Tanvir Tanjum Shourav <39719570+tanvirtanjum@users.noreply.github.com>
2021-09-04 16:52:08 +0600

    Some page added and some fixed

--- controllers/Layouts/_SharedLayoutController.js
+++ controllers/Layouts/_SharedLayoutController.js
@@ -2,5 +2,9 @@ $(document).ready(function () {
     var base_URL = "http://127.0.0.1:5500";
 
     $('#homeTab').attr("href", base_URL+"/views/Public/Home.html");
+    $('#kftTrustTab').attr("href", base_URL+"/views/Public/AboutKFTTrust.html");
+    $('#kftSchoolTab').attr("href", base_URL+"/views/Public/AboutKFTCollegiateSchool.html");
+
+    $('#conatctTab').attr("href", base_URL+"/views/Public/ContactUs.html");
     $('#loginTab').attr("href", base_URL+"/views/Public/SignIn.html");
 });
---
+++ controllers/Public/AboutKFTCollegiateSchoolController.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    var base_URL = "http://127.0.0.1:5500";
+
+    $('#topLayout').load("../Layouts/_SharedLayout.html");
+    
+});
