9e9d5763fe69dbf7ae46d4dc19db529e4027fb9c
Tanvir Tanjum Shourav <tanjumtanvir@gmail.com>
2022-05-09 02:08:23 +0600

    ./

--- Frontend/controllers/urlController.js
+++ Frontend/controllers/urlController.js
@@ -1,6 +1,4 @@
-// $(document).ready(function () {
-    
-// });
-
-var base_URL = "https://kftcs.com";
-var api_base_URL = "https://kftcs.com";
+var base_URL = "http://127.0.0.1:5500";
+var api_base_URL = "http://localhost:3000";
+// var base_URL = "https://kftcs.com";
+// var api_base_URL = "https://kftcs.com";
